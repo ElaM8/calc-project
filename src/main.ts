8adbfff7b9910a47e8e71e8a0bbb969738f25bcf
ElaM8 <ela.goodpearl@gmail.com>
2023-07-29 11:05:11 +0100

    adding function iput buttons

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,23 @@
 import './style.scss'
 
-console.log('greetings earthling')
+const buttons = document.querySelectorAll<HTMLButtonElement>("button");
+
+if (buttons.length === 0) {
+  throw new Error("Selector issue: check buttons");
+}
+
+const inputOutputDisplay = document.querySelector<HTMLDivElement>(".calculator-display");
+
+if (!inputOutputDisplay) {
+  throw new Error("Selector issue: check buttons");
+}
+
+let buttonInputs: string[] = [];
+
+const handleButtonClick = (e: Event) => { //type?
+  let inp = e.target.innerText;
+  buttonInputs.push(inp);
+  console.log(buttonInputs)
+  }
+
+buttons.forEach(b => b.addEventListener("click", handleButtonClick))
